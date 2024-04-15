e9777bf43f8fd3729ec1f393ba746978f408a20c
ajayasathya <sathyaajaya1@gmail.com>
2024-04-15 13:34:06 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed('.text', {
+        strings: ["Full Stack Developer (Java)"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true
+    });
+});
